995566e765e5c70a5287c27a1ae5c1d5c379e427
danielLoaiza <cohl.daniel@gmail.com>
2023-06-09 21:56:26 +0200

    Update CargarModelo.js

--- CargarModelo.js
+++ CargarModelo.js
@@ -2,7 +2,7 @@ import { GLTFLoader } from "./three/GLTFLoader.js";
 export async function cargarModelo(archivo, objetoVacio) {
   var loader = new GLTFLoader();
   loader.load(archivo, function (gltf) {
-    gltf.scene.scale.set(20, 20, 20);
+    gltf.scene.scale.set(3, 3, 3);
     objetoVacio.add(gltf.scene);
     console.log("cargado");
   });
